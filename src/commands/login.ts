6c7811f78f81bf7a1e7e79abaeba5cd69494e6d8
Jon Allured <jon.allured@gmail.com>
2022-02-01 14:59:16 -0600

    Switch to throwing

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -36,7 +36,7 @@ export default class Login extends Command {
         method: "POST",
       })
 
-      if (!response.ok) this.error(`${response.status} ${response.statusText}`)
+      if (!response.ok) throw `${response.status} ${response.statusText}`
 
       const data = await response.json()
 
@@ -56,11 +56,13 @@ export default class Login extends Command {
         server.close()
 
         if (query.code) {
-          const data = await getAccessToken(query.code.toString())
-
-          Config.writeToken(data.access_token)
-
-          cli.action.stop("logged in!")
+          try {
+            const data = await getAccessToken(query.code.toString())
+            Config.writeToken(data.access_token)
+            cli.action.stop("logged in!")
+          } catch (error) {
+            this.error(error)
+          }
         }
       })
       .listen(Gravity.REDIRECT_PORT)
